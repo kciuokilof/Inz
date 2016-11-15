d5e42d47e65a7a265bcfc68eacb3a8ee289d3cfe
kciuokilof <kamilkoziol@gmail.com>
2016-11-15 09:42:52 +0100

    Add files via upload

---
+++ cli.py
@@ -0,0 +1,39 @@
+
+#412  openssl genrsa -des3 -out server.orig.key 2048
+#413  openssl rsa -in server.orig.key -out server.key
+#414  openssl req -new -key server.key -out server.csr
+#415  openssl x509 -req -days 365 -in server.csr -signkey server.key -out server.crt
+
+
+import socket, ssl, pprint
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Require a certificate from the server. We used a self-signed certificate
+# so here ca_certs must be the server certificate itself.
+ssl_sock = ssl.wrap_socket(s,
+                           ca_certs="server.crt",
+                           cert_reqs=ssl.CERT_REQUIRED)
+
+ssl_sock.connect(('localhost', 10023))
+f= open('passwd','wb')
+print repr(ssl_sock.getpeername())
+print ssl_sock.cipher()
+print pprint.pformat(ssl_sock.getpeercert())
+data = ssl_sock.recv()
+f.write(data)
+f.close()
+print data
+
+
+if False: # from the Python 2.7.3 docs
+    # Set a simple HTTP request -- use httplib in actual code.
+    ssl_sock.write("""GET / HTTP/1.0\r
+    Host: www.verisign.com\n\n""")
+
+    # Read a chunk of data.  Will not necessarily
+    # read all the data returned by the server.
+    data = ssl_sock.read()
+
+    # note that closing the SSLSocket will also close the underlying socket
+    ssl_sock.close()
---
+++ serv.py
@@ -0,0 +1,30 @@
+import socket, ssl
+
+bindsocket = socket.socket()
+bindsocket.bind(('', 10023))
+bindsocket.listen(5)
+
+def do_something(connstream, data):
+    print "do_something:", data
+    return False
+
+def deal_with_client(connstream):
+    
+	f = open("passwd",'rb')
+	l=f.read(1024)
+	connstream.send(l)
+	
+	
+    
+
+while True:
+    newsocket, fromaddr = bindsocket.accept()
+    connstream = ssl.wrap_socket(newsocket,
+                                 server_side=True,
+                                 certfile="server.crt",
+                                 keyfile="server.key")
+    try:
+        deal_with_client(connstream)
+    finally:
+        connstream.shutdown(socket.SHUT_RDWR)
+        connstream.close()
